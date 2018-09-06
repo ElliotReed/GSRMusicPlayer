17eb6f484636b63bd10d7760873da352b8cab40e
gypsyelliot <elliot@elliotreed.net>
2018-09-06 11:27:44 -0600

    fetch

--- cdn/js/app.js
+++ cdn/js/app.js
@@ -1,6 +1,6 @@
 let albumData;
 
-fetch("/cdn/data/data.json")
+fetch("https://elliotreed.github.io/GSRMusicPlayer/cdn/data/data.json")
   .then(res => res.json())
   .then(data => {
     albumData = data;
